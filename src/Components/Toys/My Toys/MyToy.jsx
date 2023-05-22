955d68eb39e96c7026b924ad08f5c2eb7dab9500
jradnan <mohd.yasin085208@gmail.com>
2023-05-23 03:36:07 +0600

    single user toy has been shown

---
+++ src/Components/Toys/My Toys/MyToy.jsx
@@ -0,0 +1,68 @@
+
+const MyToy = ({toy}) => {
+    console.log(toy);
+    const {photo,name,seller,email,subCategory,price,rating,details,quantity} = toy;
+
+    return (
+        <div>
+             <div className="overflow-x-auto w-full">
+            <table className="table w-full">
+                {/* head */}
+                <thead>
+                    <tr>
+                        <th>
+                           
+                        </th>
+                        <th>Toy Name</th>
+                        <th>Seller</th>
+                        <th>Sub Category</th>
+                        <th>Price</th>
+                        <th>Available Quantity</th>
+                        <th>View Details</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {/* row 1 */}
+                    <tr>
+                        <th>
+                            <label>
+                                <input type="checkbox" className="checkbox" />
+                            </label>
+                        </th>
+                        <td>
+                            <div className="flex items-center space-x-3">
+                                <div className="avatar">
+                                    <div className="mask mask-squircle w-12 h-12">
+                                        <img src={photo} alt="Avatar Tailwind CSS Component" />
+                                    </div>
+                                </div>
+                                <div>
+                                    <div className="font-bold">{name}</div>
+                                    <div className="text-sm opacity-50">United States</div>
+                                </div>
+                            </div>
+                        </td>
+                        <td>
+                            {seller}
+                        </td>
+                        <td>{subCategory}</td>
+                        <td>
+                            {price}
+                        </td>
+                        <td>
+                            {quantity}
+                        </td>
+                        <th>
+                            <button className="btn btn-ghost btn-xs">details</button>
+                        </th>
+                    </tr>
+                </tbody>
+
+
+            </table>
+        </div>
+        </div>
+    );
+};
+
+export default MyToy;
--- src/Components/Toys/My Toys/MyToys.jsx
+++ src/Components/Toys/My Toys/MyToys.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import  { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../../Provider/AuthProvider';
+import MyToy from './MyToy';
 
 const MyToys = () => {
+
+    const {user } = useContext(AuthContext);
+    const [toyData, setToyData] = useState([])
+    const url = `http://localhost:5000/allToy?email=${user.email}`;
+    useEffect(()=>{
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setToyData(data))
+    },[])
     return (
         <div>
-            coming sooon
+           {
+                toyData.map(toy => <MyToy key={toy._id} toy={toy}></MyToy>)
+           }
         </div>
     );
 };
